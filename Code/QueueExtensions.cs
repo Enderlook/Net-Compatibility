89c9e7cf29ea9e4741274b7c5f822cb5e4e66470
Enderlook <enderlook@gmail.com>
2020-01-19 10:43:01 -0300

    Add QueueExtensions

---
+++ Code/QueueExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace System.Collections.Generic
+{
+    public static class QueueExtensions
+    {
+        public static bool TryDequeue<T>(this Queue<T> source, out T result)
+        {
+            if (source.Count > 0)
+            {
+                result = source.Dequeue();
+                return true;
+            }
+            result = default;
+            return false;
+        }
+
+        public static bool TryPeek<T>(this Queue<T> source, out T result)
+        {
+            if (source.Count > 0)
+            {
+                result = source.Peek();
+                return true;
+            }
+
+            result = default;
+            return false;
+        }
+    }
+}

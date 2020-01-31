d7387fca5b8f3b186fd5b4cdc8b01fe3a13924c2
Enderlook <enderlook@gmail.com>
2020-01-31 10:41:55 -0300

    Add StackExtensions

---
+++ Code/StackExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace System.Collections.Generic
+{
+    public static class StackExtensions
+    {
+        public static bool TryPop<T>(this Stack<T> source, out T result)
+        {
+            if (source.Count > 0)
+            {
+                result = source.Pop();
+                return true;
+            }
+            result = default;
+            return false;
+        }
+
+        public static bool TryPeek<T>(this Stack<T> source, out T result)
+        {
+            if (source.Count > 0)
+            {
+                result = source.Peek();
+                return true;
+            }
+            result = default;
+            return false;
+        }
+    }
+}

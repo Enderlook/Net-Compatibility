346ce1c6926baf7692aea45b1bac6b79ce4e4c51
Enderlook <enderlook@gmail.com>
2020-11-03 09:00:53 -0300

    Rename NetStandard21To20Compatibility to NetCompatibility, reorganice files and move KeyValuePairExtension to System.Collections.Generic namespace

---
+++ Net Compatibility/src/System/Collections/Generic/KeyValuePairExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace System.Collections.Generic
+{
+    public static class KeyValuePairExtension
+    {
+        /// <summary>
+        /// Deconstruction of <seealso cref="KeyValuePair{TKey, TValue}"/>.
+        /// </summary>
+        public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> source, out TKey key, out TValue value)
+        // https://stackoverflow.com/questions/42549491/deconstruction-in-foreach-over-dictionary
+        {
+            key = source.Key;
+            value = source.Value;
+        }
+    }
+}
